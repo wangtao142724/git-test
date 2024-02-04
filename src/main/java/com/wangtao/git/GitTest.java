3107c7d86c56f00d03fb8a6681ffd4d2c926c5a9
wangtao <wangtao>
2024-02-04 16:40:02 +0800

    hello git！

---
+++ src/main/java/com/wangtao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wangtao.git;
+
+/**
+ * @author wangtao
+ * @create 2024-02-04 16:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
